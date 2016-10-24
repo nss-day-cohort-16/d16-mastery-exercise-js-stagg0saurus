c8933cdb7c390b310bdc657d75661935e5608e5f
Peter Staggs <peter.staggs@csuglobal.edu>
2016-10-24 14:09:30 -0500

    .

--- quizjs.js
+++ quizjs.js
@@ -1,5 +1,7 @@
 //Created an empty object. 
 var treeOb = {}
+//create a var for the submit button
+var growBTN =document.getElementById("grow");
 //Created a single function with two conditions. 
 function runInput() {
 	//Defined object key values with input from DOM.
@@ -16,13 +18,25 @@ function runInput() {
 			var hardMath = 2 * i + 1; //the first line is 1, you want the next line to be 3 (2x1+1), and so on.
 			console.log(space.repeat(spaceMath) + treeOb.char.repeat(hardMath)); //spaces repeating plus chars repeating
 			}
-¿		}
-		return false; /*By default,"onSubmit" returns a value of true, submits
+		}
+		/*return false; /* Initially, I used a form with 'onSubmit' to forego using event listeners. 
+		By default,"onSubmit" returns a value of true, submits
 		the form, and resets the page/console. This initially kept me from displaying my tree. 
-		To prevent this, I set my function to return false. 
-		Using "onSubmit" in the html form bypasses the need for event listeners.*/
+		To prevent this, I set my function to return false. */
 	}
-
+//Add event listeners for mouseclick and enter. 
+growBTN.addEventListener("click", runInput); 
+document.getElementById("inputH").addEventListener("keydown", function(btn) {
+	if (btn.keyCode == 13) {
+		runInput(); 
+	}
+});
+document.getElementById("inputC").addEventListener("keydown",
+function(btn2) {
+	if (btn2.keyCode == 13) {
+		runInput(); 
+	}
+});
 
 
 
